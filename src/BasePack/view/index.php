56d0fb988e34bd62070b0921aa695821d7c302a0
Jarzon <j@jasonvaillancourt.ca>
2018-02-14 21:42:51 -0500

    WIP Show projects based on an array

--- src/BasePack/view/index.php
+++ src/BasePack/view/index.php
@@ -1,3 +1,8 @@
 <?php $this->start('default') ?>
+    <?php foreach ([
+        ['Libellum', 'http://libellum.localhost', 'https://libellum.ca', 'https://github.com/Jarzon/Libellum']
+                   ] as list($name, $devUrl, $prodUrl, $repoUrl)): ?>
+    <?=$name?>: <a href="<?=$devUrl?>" target="_top"><?=$devUrl?></a> - <a href="<?=$prodUrl?>" target="_top"><?=$prodUrl?></a> - <a href="<?=$repoUrl?>" target="_top"><?=$repoUrl?></a><br>
+    <?php endforeach; ?>
 
 <?php $this->end() ?>
