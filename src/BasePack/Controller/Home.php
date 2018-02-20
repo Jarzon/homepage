f8e9fe4e906b76393fe80c0dbb3317c7a46c5bd2
Jarzon <j@jasonvaillancourt.ca>
2018-02-20 14:20:35 -0500

    Refactor project list

--- src/BasePack/Controller/Home.php
+++ src/BasePack/Controller/Home.php
@@ -19,79 +19,149 @@ class Home extends Controller
         ]);
 
         $projects = [
-            ['Libellum', ['http://libellum.localhost/', 'https://libellum.ca/', 'https://github.com/Jarzon/Libellum/']],
-            ['JV', ['http://webagency.localhost/', 'https://jasonvaillancourt.ca/', 'https://github.com/Jarzon/webagency/']],
-            ['MasterJ', ['http://masterj.localhost/', 'https://masterj.net/', 'https://github.com/Jarzon/Masterj/']],
-            ['Tasks', ['http://tasks.localhost/', 'https://tasks.masterj.net/', 'https://github.com/Jarzon/Tasks/']],
-            ['Game', ['http://localhost/game/', 'https://www.masterj.net/game/', 'https://github.com/Jarzon/RPG/']],
-        ];
+            // Websites
 
-        $phpunit = [
-            'Prim',
-            'Cache',
-            'Forms',
-            'Localization',
-            'Pagination',
-        ];
+            'Libellum' => [
+                'location' => 'Libellum/',
+                'dev' => 'http://libellum.localhost/',
+                'prod' => 'https://libellum.ca/',
+                'github' => 'https://github.com/Jarzon/Libellum/'
+            ],
+            'JV' => [
+                'location' => 'webagency/',
+                'dev' => 'http://webagency.localhost/',
+                'prod' => 'https://jasonvaillancourt.ca/',
+                'github' => 'https://github.com/Jarzon/webagency/'
+            ],
+            'MasterJ' => [
+                'location' => 'masterj/',
+                'dev' => 'http://masterj.localhost/',
+                'prod' => 'https://masterj.net/',
+                'github' => 'https://github.com/Jarzon/Masterj/'
+            ],
+            'Tasks' => [
+                'location' => 'tasks/',
+                'dev' => 'http://tasks.localhost/',
+                'prod' => 'https://tasks.ca/',
+                'github' => 'https://github.com/Jarzon/Tasks/'
+            ],
+            'Game' => [
+                'location' => 'game/',
+                'dev' => 'http://localhost/game/',
+                'prod' => 'https://www.masterj.net/game/',
+                'github' => 'https://github.com/Jarzon/RPG/'
+            ],
 
-        $pastProjects = [
-            ['Berlos', ['http://localhost/archived/Berlos/', 'https://github.com/Jarzon/Berlos/']],
-            ['JVA', ['http://localhost/archived/JVA/index.htm', 'https://github.com/Jarzon/JVA/']],
-            ['Assur-Info', ['http://localhost/archived/assur-info/web/app.php/', 'https://github.com/Jarzon/Assur-Info/']],
-            ['Assur-Price', ['http://localhost/archived/assur-price/web/', 'https://github.com/Jarzon/Assur-Price/']],
-            ['Dragon-Lord', ['http://localhost/archived/Dragon-Lord/web/', 'https://github.com/Jarzon/dragon-lord/']],
-            ['ebl', ['http://localhost/archived/ebl/web/', 'https://github.com/Jarzon/ebl/']],
-            ['stats', ['http://localhost/archived/stats/', 'https://github.com/Jarzon/stats/']],
-        ];
+            // Services
 
-        $motds = [
-            ['motd', ['http://motd.localhost/', 'https://github.com/Jarzon/motd/']],
-            ['PAP', ['http://ub.localhost/', 'https://github.com/Jarzon/ub/']],
-            ['Yoshi3', ['http://yoshi3.localhost/', 'https://github.com/Jarzon/yoshi3/']],
-            ['Omnis', ['http://oh.localhost/', 'https://github.com/Jarzon/OmnisHaven/']],
+            'Prim' => [
+                'location' => 'Services/Prim/',
+                'dev' => 'http://localhost/phpunit/prim/',
+                'github' => 'https://github.com/Jarzon/Prim/'
+            ],
+            'Cache' => [
+                'location' => 'Services/Cache/',
+                'dev' => 'http://localhost/phpunit/cache/',
+                'github' => 'https://github.com/Jarzon/Cache/'
+            ],
+            'Forms' => [
+                'location' => 'Services/Forms/',
+                'dev' => 'http://localhost/phpunit/forms/',
+                'github' => 'https://github.com/Jarzon/Forms/'
+            ],
+            'Localization' => [
+                'location' => 'Services/Localization/',
+                'dev' => 'http://localhost/phpunit/localization/',
+                'github' => 'https://github.com/Jarzon/Localization/'
+            ],
+            'Pagination' => [
+                'location' => 'Services/Pagination/',
+                'dev' => 'http://localhost/phpunit/pagination/',
+                'github' => 'https://github.com/Jarzon/Pagination/'
+            ],
+            'Capture' => [
+                'location' => 'Services/capture/',
+                'dev' => 'http://localhost/phpunit/capture/',
+                'github' => 'https://github.com/Jarzon/Capture/'
+            ],
+
+            // Past Projects
+
+            'Berlos' => [
+                'location' => 'archived/Berlos/',
+                'dev' => 'http://localhost/archived/Berlos/',
+                'github' => 'https://github.com/Jarzon/Berlos/'
+            ],
+            'JVA' => [
+                'location' => 'archived/JVA/',
+                'dev' => 'http://localhost/archived/JVA/index.htm',
+                'github' => 'https://github.com/Jarzon/JVA/'
+            ],
+            'Assur-Info' => [
+                'location' => 'archived/assur-info/',
+                'dev' => 'http://localhost/archived/assur-info/web/app.php/',
+                'github' => 'https://github.com/Jarzon/Assur-Info/'
+            ],
+            'Assur-Price' => [
+                'location' => 'archived/assur-price/',
+                'dev' => 'http://localhost/archived/assur-price/web/',
+                'github' => 'https://github.com/Jarzon/Assur-Price/'
+            ],
+            'Dragon-Lord' => [
+                'location' => 'archived/dragon-lord/',
+                'dev' => 'http://localhost/archived/Dragon-Lord/web/',
+                'github' => 'https://github.com/Jarzon/dragon-lord/'
+            ],
+            'ebl' => [
+                'location' => 'archived/ebl/',
+                'dev' => 'http://localhost/archived/ebl/web/',
+                'github' => 'https://github.com/Jarzon/ebl/'
+            ],
+            'stats' => [
+                'location' => 'archived/stats/',
+                'dev' => 'http://localhost/archived/stats/',
+                'github' => 'https://github.com/Jarzon/stats/'
+            ],
+
+            // Motds
+
+            'motd' => [
+                'location' => 'motds/motd',
+                'dev' => 'http://motd.localhost/',
+                'github' => 'https://github.com/Jarzon/motd/'
+            ],
+            'PAP' => [
+                'location' => 'motds/ub',
+                'dev' => 'http://ub.localhost/',
+                'github' => 'https://github.com/Jarzon/ub/'
+            ],
+            'Yoshi3' => [
+                'location' => 'motds/yoshi3',
+                'dev' => 'http://yoshi3.localhost/',
+                'github' => 'https://github.com/Jarzon/yoshi3/'
+            ],
+            'Omnis' => [
+                'location' => 'motds/omnishaven',
+                'dev' => 'http://oh.localhost/',
+                'prod' => 'https://www.masterj.net/game/',
+                'github' => 'https://github.com/Jarzon/OmnisHaven/'
+            ],
         ];
 
         $capture = new Capture([
             'chrome_path' => (Capture::isWindows())? '"C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"': '/usr/bin/google-chrome'
         ]);
 
-        foreach ($projects as list($name, $urls)) {
-            $cache->registerCache($name, 3600, function($name) use($capture, $urls) {
-                $capture->screenshot($urls[0], "{$this->options['app']}cache/preview/$name.png", false);
-
-                return true;
-            });
-        }
-
-        foreach ($phpunit as $name) {
-            $cache->registerCache($name, 3600, function($name) use($capture) {
-                $capture->screenshot("http://localhost/phpunit/$name/", "{$this->options['app']}cache/preview/$name.png", false);
-
-                return true;
-            });
-        }
-
-        foreach ($pastProjects as list($name, $urls)) {
-            $cache->registerCache($name, 3600, function($name) use($capture, $urls) {
-                $capture->screenshot($urls[0], "{$this->options['app']}cache/preview/$name.png", false);
-
-                return true;
-            });
-        }
-
-        foreach ($motds as list($name, $urls)) {
-            $cache->registerCache($name, 3600, function($name) use($capture, $urls) {
-                $capture->screenshot($urls[0], "{$this->options['app']}cache/preview/$name.png", false);
+        foreach ($projects as $name => $infos) {
+            $cache->registerCache($name, 86400, function($name) use($capture, $infos) {
+                $capture->screenshot($infos['dev'], "{$this->options['app']}cache/preview/$name.png");
 
                 return true;
             });
         }
 
         $this->render('index', 'BasePack', [
-            'projects' => $projects,
-            'pastProjects' => $pastProjects,
-            'phpunit' => $phpunit,
-            'motds' => $motds
+            'projects' => $projects
         ]);
     }
 }
--- src/BasePack/view/index.php
+++ src/BasePack/view/index.php
@@ -3,47 +3,17 @@
 <?php $this->start('default') ?>
 
 <h2>Projects</h2>
-<?php foreach ($projects as list($name, $urls)): ?>
+<?php foreach ($projects as $name => $infos): ?>
     <div class="item">
         <div class="preview">
-            <a href="<?=$urls[0]?>" target="_top"><img src="<?=$this->fileCache("/img/preview/$name.png")?>"></a>
+            <a href="<?=$infos['dev']?>" target="_top"><img src="<?=$this->fileCache("/img/preview/$name.png")?>"></a>
         </div>
         <div class="links">
-            <a href="<?=$urls[1]?>" target="_top">Website</a> - <a href="<?=$urls[2]?>" target="_top">GitHub repository</a>
-        </div>
-    </div>
-<?php endforeach; ?>
-
-<h2>PHPUnit Coverage</h2>
-<?php foreach ($phpunit as $name): ?>
-    <div class="item">
-        <h3><?=$name?></h3>
-        <div class="preview">
-            <a href="http://localhost/phpunit/<?=$name?>" target="_top"><img src="<?=$this->fileCache("/img/preview/$name.png")?>"></a>
-        </div>
-    </div>
-<?php endforeach; ?>
-
-    <h2>Past projects</h2>
-<?php foreach ($pastProjects as list($name, $urls)): ?>
-    <div class="item past">
-        <div class="preview">
-            <a href="<?=$urls[0]?>" target="_top"><img src="<?="/img/preview/$name.png"?>"></a>
-        </div>
-        <div class="links">
-            <a href="<?=$urls[1]?>" target="_top">GitHub repository</a>
-        </div>
-    </div>
-<?php endforeach; ?>
-
-<h2>Motds</h2>
-<?php foreach ($motds as list($name, $urls)): ?>
-    <div class="item past">
-        <div class="preview">
-            <a href="<?=$urls[0]?>" target="_top"><img src="<?="/img/preview/$name.png"?>"></a>
-        </div>
-        <div class="links">
-            <a href="<?=$urls[1]?>" target="_top">GitHub repository</a>
+            <?php if(isset($infos['prod'])):?>
+                <a href="<?=$infos['prod']?>" target="_top">Website</a> -
+            <?php endif ?>
+            <a href="/<?=$name?>" target="_top">PHPStorm</a> -
+            <a href="<?=$infos['github']?>" target="_top">GitHub</a>
         </div>
     </div>
 <?php endforeach; ?>
